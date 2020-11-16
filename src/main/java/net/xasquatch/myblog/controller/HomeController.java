bfd7232981dd81dc0be47e3e9c7a9eddcc624e04
xasquatch <bigfoot7774@gmail.com>
2020-11-17 02:46:41 +0900

    -익셉션전용 컨트롤러 어드바이저를 만들어 커스텀 에러 페이지를 구현 하려고 시도하였다. 서버에러( 5xx번대)는 처리가 되었지만, 디스패처 서블릿 외부에서 발생하는 404에러는 잡질 못했다. web.xml에 에러페이지를 추가하면 되는 쉬운문제였는데 난 web.xml을 절대 쓰기 싫었다 정말 이미 webapplicationInitializer 클래스를 상속 받아 web.xml을 대신 자바코드로 전부 활용하려 하고 있었고, 이 상황에서 web.xml을 에러페이지때문에 추가한다는 건 내 방향성을 깨는 일이였다. web.xml을 대체할 404에러 처리 방법을 다 찾아보았다. 몇 시간 삽질해보았지만 답이 안나와 결국 일단 web.xml을 만들었다.(물론 잘됨) 비참하다. web.xml은 대안이 나오면 결국 꼭 지울 거다 반드시

--- src/main/java/net/xasquatch/myblog/controller/HomeController.java
+++ src/main/java/net/xasquatch/myblog/controller/HomeController.java
@@ -2,21 +2,14 @@ package net.xasquatch.myblog.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import net.xasquatch.myblog.model.Member;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.NoHandlerFoundException;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @Controller
-@ControllerAdvice
 public class HomeController {
 
     @Resource(name = "sessionMember")
@@ -45,13 +38,6 @@ public class HomeController {
         model.addAttribute("mainContents", "login");
 
         return "index";
-    }
-
-    //  TODO:에러 처리 임시
-    @ExceptionHandler(NoHandlerFoundException.class)
-    @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    public String requestHandlingNoHandlerFound(HttpServletRequest req, NoHandlerFoundException ex) {
-        return "error404";
     }
 
 }
