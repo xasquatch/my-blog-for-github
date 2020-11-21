1539a09f7bca8f55144e47a5604665ac79aaa8da
xasquatch <bigfoot7774@gmail.com>
2020-11-21 17:04:44 +0900

    매퍼 contents convert처리 및 ip 마스킹 처리

--- src/main/java/net/xasquatch/myblog/mapper/ApiMapper.java
+++ src/main/java/net/xasquatch/myblog/mapper/ApiMapper.java
@@ -16,6 +16,11 @@ public interface ApiMapper {
     @Select("SELECT COUNT(*) as total_count FROM board WHERE member_no = #{arg0} AND completed = 'true'")
     int selectBoardCount(Object memberNo);
 
-    @Select("SELECT * FROM board WHERE member_no = #{arg0} AND no = #{arg1}")
+    @Select("SELECT no, member_no, keyword, title, " +
+            "convert(contents USING UTF8) as contents, " +
+            "created_date, thumbnail, " +
+            "REPLACE(created_ip, RIGHT(created_ip,5),'*****') AS created_ip " +
+            "FROM board " +
+            "WHERE member_no = #{arg0} AND no = #{arg1}")
     Map<String, Object> selectBoardOne(String memberNo, String boardNo);
 }
