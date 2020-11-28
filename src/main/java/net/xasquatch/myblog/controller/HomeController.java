7b10932da3c45822920550f54331498d4d68bc46
xasquatch <bigfoot7774@gmail.com>
2020-11-29 00:19:13 +0900

    no message

--- src/main/java/net/xasquatch/myblog/controller/HomeController.java
+++ src/main/java/net/xasquatch/myblog/controller/HomeController.java
@@ -2,15 +2,25 @@ package net.xasquatch.myblog.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import net.xasquatch.myblog.model.Member;
+import net.xasquatch.myblog.service.ImgService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Slf4j
 @Controller
 public class HomeController {
+
+    @Autowired
+    private ImgService imgService;
 
     @Resource(name = "sessionMember")
     private Member sessionMember;
@@ -39,5 +49,16 @@ public class HomeController {
 
         return "index";
     }
+
+
+    @PostMapping("/img/{memberNo}/board/{boardNo}/upload")
+    @ResponseBody
+    public String boardImgUpload(MultipartHttpServletRequest request, @PathVariable String memberNo, @PathVariable String boardNo) {
+
+        List<String> resultList = imgService.uploadImage(request, memberNo, boardNo);
+
+        return resultList.toString();
+    }
+
 
 }
