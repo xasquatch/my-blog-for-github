2db2b17cc9f91895f25e2edeae15953ee0272fb8
xasquatch <bigfoot7774@gmail.com>
2020-11-29 06:44:46 +0900

    boardMapper 게시글목록 조회 구현 완료
    TODO LIST
    - 게시글 상세조회 및 도메인 정리

--- src/main/java/net/xasquatch/myblog/controller/BoarderController.java
+++ src/main/java/net/xasquatch/myblog/controller/BoarderController.java
@@ -98,6 +98,16 @@ public class BoarderController {
 
         model.addAttribute("boardList", boardList);
         model.addAttribute("pageBlockList", pageBlockList);
+        model.addAttribute("pageLimit", pageLimit);
+
+        try {
+            model.addAttribute("searchTarget", searchValue[0]);
+            model.addAttribute("searchValue", searchValue[1].substring(1, searchValue[1].length() - 1));
+
+        }catch (NullPointerException e){
+            log.debug("viewAllList {}",e.getMessage());
+            model.addAttribute("searchTarget", "keyword");
+        }
 
         model.addAttribute("mainContents", "board-view-list-all");
         return "index";
@@ -105,14 +115,28 @@ public class BoarderController {
 
     //TODO: 게시판 리스트 조회 페이지
     @RequestMapping(value = "/{memberNo}/list", method = {RequestMethod.GET, RequestMethod.POST})
-    public String viewList(Model model, @PathVariable String memberNo) {
-        model.addAttribute("mainContents", "board-view-list");
+    public String viewList(Model model, @PathVariable String memberNo,
+                           @RequestParam(value = "page-limit", required = false, defaultValue = "10") int pageLimit,
+                           @RequestParam(value = "current-page-block", required = false, defaultValue = "1") int currentPageBlock,
+                           @RequestParam(value = "keyword", required = false, defaultValue = "") String keyword,
+                           @RequestParam(value = "title", required = false, defaultValue = "") String title,
+                           @RequestParam(value = "contents", required = false, defaultValue = "") String contents,
+                           @RequestParam(value = "title-or-contents", required = false, defaultValue = "") String titleOrContents) {
 
+        String[] searchValue = boardService.parsingSearchValue(keyword, title, contents, titleOrContents);
+        Map<String, Object> boardUnit = boardService.getBoardList(memberNo, pageLimit, currentPageBlock, searchValue);
+
+        List<Map<String, Object>> boardList = (List<Map<String, Object>>) boardUnit.get("boardList");
+        Object pageBlockList = boardUnit.get("pageBlockList");
+
+        model.addAttribute("boardList", boardList);
+        model.addAttribute("pageBlockList", pageBlockList);
+        model.addAttribute("mainContents", "board-view-list");
 
         return "index";
     }
 
-    @RequestMapping(value = "/{memberNo}/detail/{boardNo}", method = {RequestMethod.GET, RequestMethod.POST})
+    @RequestMapping(value = "/{memberNo}/read/{boardNo}", method = {RequestMethod.GET, RequestMethod.POST})
     public String viewDetail(Model model, @PathVariable String boardNo, @PathVariable String memberNo) {
 
         model.addAttribute("boardNo", boardNo);
