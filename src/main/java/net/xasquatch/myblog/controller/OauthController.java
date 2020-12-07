ccbbba03a61566cab80c05b6a15b9301b529025d
xasquatch <bigfoot7774@gmail.com>
2020-12-08 02:44:16 +0900

    로그인까지 구현 TODO LIST - 회원정보수정이 안됨 : 회원정보 수정 방법을 모색할 필요성이 있음(패스워드 임의값) - 토큰 쿠키에 저장방법 구상필요 - 깔끔한 리다이렉트 필요

--- src/main/java/net/xasquatch/myblog/controller/OauthController.java
+++ src/main/java/net/xasquatch/myblog/controller/OauthController.java
@@ -1,41 +1,84 @@
 package net.xasquatch.myblog.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import net.xasquatch.myblog.model.Member;
 import net.xasquatch.myblog.service.GoogleOAuthService;
+import net.xasquatch.myblog.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.HashMap;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.util.Map;
 
 @Slf4j
 @Controller
-@RequestMapping(value = "/oauth", method = {RequestMethod.GET})
+@RequestMapping(value = "/oauth",
+        method = {RequestMethod.GET},
+        produces = "text/plain;charset=UTF-8")
 public class OauthController {
 
     @Autowired
     private GoogleOAuthService googleOAuthService;
 
+    @Autowired
+    private MemberService memberService;
+
+    @Resource(name = "sessionMember")
+    private Member sessionMember;
+
     @GetMapping("/google/information")
     @ResponseBody
     public String googleAuthInfo() {
-
         return googleOAuthService.getGoogleInfo();
 
     }
 
-    @GetMapping("/google")
-    public String googleSignUpAndIn(Model model, @RequestParam(value = "code") String authCode) {
-
+    @PostMapping("/google/token")
+    @ResponseBody
+    public String login(@RequestParam(value = "google-token") String JWT) {
         String result = null;
+
         try {
-            result = googleOAuthService.signUpAndIn(authCode);
+            Member member = googleOAuthService.verifyToken(JWT);
+            if (member == null) return "false";
+            result = memberService.loginForToken(member);
+
+        } catch (GeneralSecurityException | IOException e) {
+            log.warn("OAuth JsonProcessingException : GOOGLE [{}]", "verify");
+        }
+
+        return result;
+    }
+
+    @GetMapping("/google")
+    public String googleSignUpAndIn(Model model, HttpSession session, @RequestParam(value = "code") String authCode) {
+        String result = null;
+
+        try {
+            ResponseEntity<String> responseEntity = googleOAuthService.requestAccessToken(authCode);
+            Map<String, Object> accessTokenMap = googleOAuthService.getAccessToken(responseEntity);
+
+            Member convertedMember = googleOAuthService.convertModelToMember(accessTokenMap);
+            if (memberService.isExistedEmail(convertedMember.getEmail())) {
+                memberService.updateRank(sessionMember.getEmail());
+                memberService.loginForToken(convertedMember);
+                session.setAttribute("sessionMember", sessionMember);
+                result = "이메일이 이미 있습니다.";
+
+            }else{
+                memberService.saveForToken(convertedMember);
+                memberService.updateRank(sessionMember.getEmail());
+                memberService.loginForToken(convertedMember);
+                session.setAttribute("sessionMember", sessionMember);
+            }
+
         } catch (JsonProcessingException e) {
             log.warn("OAuth JsonProcessingException : GOOGLE [{}]", "googleSignUpAndIn");
         }
@@ -44,7 +87,7 @@ public class OauthController {
         return "forward:/";
     }
 
-    @GetMapping("google/token")
+    @DeleteMapping("/google/token")
     public String revokeGoogleToken(Model model, @RequestParam(value = "token") String token) {
 
         String result = null;
