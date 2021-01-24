f8311fb774c84ce3cea4d4437fb1d68a2610402f
xasquatch <bigfoot7774@gmail.com>
2021-01-24 21:31:37 +0900

    코멘트 매퍼 구현중

---
+++ src/main/java/net/xasquatch/myblog/mapper/builder/CommentBulider.java
@@ -0,0 +1,30 @@
+package net.xasquatch.myblog.mapper.builder;
+
+import org.apache.ibatis.jdbc.SQL;
+
+public class CommentBulider {
+
+    public static String selectCommentList(Object boardNo, Object currentPage){
+
+        return new SQL(){{
+            SELECT("");
+        }}.toString();
+    }
+
+    public static String selectCountComment(Object boardNo, Object currentPage){
+
+        return new SQL(){{
+            SELECT("COUNT(*) AS total_count");
+            FROM("COMMENT");
+        }}.toString();
+    }
+
+    public static String selectCommentListSubQuery(Object boardNo, Object currentPage){
+
+        return new SQL(){{
+            SELECT("FORMAT(@ROWNUM := @ROWNUM + 1, 0) AS row_number");
+        }}.toString();
+
+
+    }
+}
