2dd1cb6d45bb84efae4a377e987de2d81968c314
xasquatch <bigfoot7774@gmail.com>
2021-01-24 20:09:03 +0900

    comment 모델 클래스 추가

---
+++ src/main/java/net/xasquatch/myblog/model/Comment.java
@@ -0,0 +1,24 @@
+package net.xasquatch.myblog.model;
+
+import lombok.*;
+
+import javax.validation.constraints.Size;
+
+@Builder
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+
+    private Long no;
+    private Long mbr_no;
+    private String mbr_nickname;
+
+    @Size(max = 255)
+    private String contents;
+    private String created_date;
+    private String created_ip;
+
+
+}
