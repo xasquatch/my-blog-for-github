955a2f1e4aa44686a21ba25aa3ee62f0589c5f19
xasquatch <bigfoot7774@gmail.com>
2021-02-13 16:51:15 +0900

    코멘트 리스트 출력 빌더 준비중

--- src/main/java/net/xasquatch/myblog/mapper/builder/CommentBuilder.java
+++ src/main/java/net/xasquatch/myblog/mapper/builder/CommentBuilder.java
@@ -1,30 +1,33 @@
 package net.xasquatch.myblog.mapper.builder;
 
+import net.xasquatch.myblog.model.Comment;
 import org.apache.ibatis.jdbc.SQL;
 
 public class CommentBuilder {
 
-    public static String selectCommentList(Object boardNo, Object currentPage){
-
-        return new SQL(){{
+    public static String selectCommentList(Object boardNo, Object currentPage) {
+        return new SQL() {{
             SELECT("");
         }}.toString();
+
     }
 
-    public static String selectCountComment(Object boardNo, Object currentPage){
-
-        return new SQL(){{
+    public static String selectCountComment(Object boardNo, Object currentPage) {
+        return new SQL() {{
             SELECT("COUNT(*) AS total_count");
-            FROM("COMMENT");
+            FROM("COMMENT c, (SELECT @ROWNUM := 0) TMP");
+            WHERE("boardNo = " + boardNo);
         }}.toString();
+
     }
 
-    public static String selectCommentListSubQuery(Object boardNo, Object currentPage){
-
-        return new SQL(){{
+    public static String selectCommentListSubQuery(Object boardNo, Object currentPage) {
+        return new SQL() {{
             SELECT("FORMAT(@ROWNUM := @ROWNUM + 1, 0) AS row_number");
+            FROM("COMMENT c, (SELECT @ROWNUM := 0) TMP");
+            WHERE("boardNo = " + boardNo);
         }}.toString();
 
-
     }
+
 }
