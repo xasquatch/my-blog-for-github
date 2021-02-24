cc4926952af3739e9df129cfbd666c4c0b42a74c
xasquatch <bigfoot7774@gmail.com>
2021-02-25 01:23:22 +0900

    ajax contentsType: if문 변경(else추가)

--- src/main/webapp/resources/js/myblog-api.js
+++ src/main/webapp/resources/js/myblog-api.js
@@ -5,7 +5,7 @@ var myAjax = {
     formFile: 'multipart/form-data',
 
     setContentsType: function (inputContentsType) {
-        var contentsType = 'text/plain';
+        var contentsType;
 
         if (inputContentsType.toUpperCase() === 'FORM') {
             contentsType = myAjax.form;
@@ -15,6 +15,9 @@ var myAjax = {
 
         } else if (inputContentsType.toUpperCase() === 'JSON') {
             contentsType = myAjax.json;
+
+        } else{
+            contentsType = 'text/plain';
 
         }
         return contentsType;
