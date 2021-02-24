bf9f9ed90fde1896b0d22926de5ad3cd168cf5e6
xasquatch <bigfoot7774@gmail.com>
2021-02-25 00:36:11 +0900

    rownum 없애고 기본키값(long형,  zerofil)로 역정렬하여 sql단순화

--- src/main/java/net/xasquatch/myblog/mapper/builder/CommentBuilder.java
+++ src/main/java/net/xasquatch/myblog/mapper/builder/CommentBuilder.java
@@ -6,13 +6,12 @@ public class CommentBuilder {
 
     public static String selectCommentList(Object boardNo) {
         return new SQL() {{
-            SELECT("row_number, no, mbr_no, " +
-                    "convert(contents USING UTF8) AS contents," +
+            SELECT("no, mbr_no, convert(contents USING UTF8) AS contents," +
                     "REPLACE(created_ip, RIGHT(created_ip, 4),'.***') AS created_ip," +
                     "DATE_FORMAT(created_date, '%Y.%m.%d %H:%i:%s') AS created_date," +
                     "board_no, mbr_name, img");
-            FROM("(" + selectCommentListSubQuery(boardNo) + ") c, (SELECT @ROWNUM := 0) TMP");
-            ORDER_BY("length(row_number) DESC, row_number DESC");
+            FROM("(" + selectCommentListSubQuery(boardNo) + ") c");
+            ORDER_BY("no DESC");
         }}.toString();
 
     }
@@ -28,8 +27,7 @@ public class CommentBuilder {
 
     public static String selectCommentListSubQuery(Object boardNo) {
         return new SQL() {{
-            SELECT("FORMAT(@ROWNUM := @ROWNUM + 1, 0) AS row_number, " +
-                    "c.no AS no, c.mbr_no AS mbr_no, " +
+            SELECT("c.no AS no, c.mbr_no AS mbr_no, " +
                     "m.name AS mbr_name, m.img AS img, " +
                     "c.contents AS contents, c.created_ip AS created_ip, " +
                     "c.created_date AS created_date, " +
