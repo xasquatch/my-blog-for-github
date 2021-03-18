a608691c3019a1a131ef81667b2d7a55e7590adf
xasquatch <bigfoot7774@gmail.com>
2021-03-18 09:11:20 +0900

    like테이블 연계하여 게시판과 댓글 조회하는 매퍼 임시추가

--- src/main/java/net/xasquatch/myblog/mapper/builder/LikeBuilder.java
+++ src/main/java/net/xasquatch/myblog/mapper/builder/LikeBuilder.java
@@ -4,15 +4,47 @@ import org.apache.ibatis.jdbc.SQL;
 
 public class LikeBuilder {
 
-    public static String selectOneLike(Object likeNo, Object memberNo) {
+    /**
+     * @param likeTarget 분기점
+     * @param likeNo WHERE 조건절
+     *
+     * @return
+     */
+    public static String selectOneLike(String likeTarget, Object likeNo) {
         return new SQL() {{
-            SELECT("no, mbr_no, board_no, comment_no, good");
-            FROM("lke");
-            WHERE("no= " + likeNo + " AND mbr_no = " + memberNo);
+            if (likeTarget.toUpperCase().equals("BOARD")) {
+                SELECT("l.no AS no, " +
+                        "l.mbr_no AS mbr_no, " +
+                        "l.board_no AS board_no, " +
+                        "l.good AS like, " +
+                        "b.title AS board_title, " +
+                        "b.thumbnail AS board_thumbnail");
+                FROM("lke l ON");
+                JOIN("board b");
+
+            } else if (likeTarget.toUpperCase().equals("COMMENT")) {
+                SELECT("l.no AS no, " +
+                        "l.mbr_no AS mbr_no, " +
+                        "l.comment_no AS comment_no, " +
+                        "l.good AS like, " +
+                        "c.contents AS comment_contents, " +
+                        "c.board_no AS comment_board_no");
+                FROM("lke l ON");
+                JOIN("comment c");
+            }
+            WHERE("no= " + likeNo);
         }}.toString();
 
     }
 
+    /**
+     *
+     * @param likeTarget 분기점 생성
+     * @param targetNo 조건절에서 분기된 해당 테이블 외래키값으로 삽입할 파라미터
+     * @param memberNo 멤버테이블 파라미터
+     * @param goodValue 1 혹은 -1 예정 삽입
+     * @return
+     */
     public static String insertOneLike(String likeTarget, String targetNo, String memberNo, String goodValue) {
         return new SQL() {{
             INSERT_INTO("lke");
